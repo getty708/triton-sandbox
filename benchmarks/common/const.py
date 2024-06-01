5ceb4a6910c9a86cee022b32b1edcaab95de4dfb
Naoya Yoshimura <yoshimura708x@gmail.com>
2024-06-01 15:33:33 +0000

    feat: setup minimum pipeline

--- benchmarks/common/const.py
+++ benchmarks/common/const.py
@@ -1,2 +1,8 @@
+from pathlib import Path
+
 FULLHD_WIDTH = 1920
 FULLHD_HEIGHT = 1080
+
+
+_REPO_ROOT = Path(__file__).parents[2]
+SAMPLE_IMAGE_PATH = _REPO_ROOT / "data" / "pexels-anna-tarazevich-14751175-fullhd.jpg"
