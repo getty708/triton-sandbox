e12bd4b57b95a89c97b3f30047b2f88036a5d772
Naoya Yoshimura <yoshimura708x@gmail.com>
2024-06-01 14:54:16 +0000

    feat: model concurrency execution

---
+++ benchmarks/common/models/simple_cnn/const.py
@@ -0,0 +1,2 @@
+SIMPLE_CNN_INPUT_TENSOR = "input_image"
+SIMPLE_CNN_OUTPUT_TENSOR = "output_image"
---
+++ benchmarks/common/models/simple_cnn/simple_cnn.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from benchmarks.common.models.simple_cnn.const import SIMPLE_CNN_OUTPUT_TENSOR
+
+
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 3, (3, 3), stride=1, padding=(1, 1))
+        self.conv2 = nn.Conv2d(3, 3, (3, 3), stride=1, padding=(1, 1))
+        self.conv3 = nn.Conv2d(3, 3, (3, 3), stride=1, padding=(1, 1))
+
+    def forward(self, image_tensor: torch.Tensor) -> dict[str, torch.Tensor]:
+        x = F.relu(self.conv1(image_tensor))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        return {SIMPLE_CNN_OUTPUT_TENSOR: x}
